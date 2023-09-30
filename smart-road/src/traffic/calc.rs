d956d520fd598673c1b3afeec2440539ac281f3a
healingdrawing <healingdrawing@gmail.com>
2023-09-30 03:33:29 +0300

    wip implement way, autos, calc

--- smart-road/src/traffic/calc.rs
+++ smart-road/src/traffic/calc.rs
@@ -24,9 +24,13 @@ impl<'a> Calc<'a> {
 
   pub fn update(&mut self) {
     self.autos.ss.iter_mut().for_each(|auto| auto.animate_step());
-    println!("update");
+    // println!("update");
   }
   
+  fn auto_targeted_to_point_does_not_move(&self, point:&[u16; 2], lane_autos:&LimitedStack<Auto>) -> bool {
+    lane_autos.iter().any(|auto| auto.to_x == point[0] as f32 && auto.to_y == point[1] as f32 && auto.moving == false)
+  }
+
   fn no_autos_targeted_to_point(&self, point:&[u16; 2], lane_autos:&LimitedStack<Auto>) -> bool {
     lane_autos.iter().all(|auto| auto.to_x != point[0] as f32 && auto.to_y != point[1] as f32)
   }
@@ -73,7 +77,7 @@ impl<'a> Calc<'a> {
     // todo
     match road_direction {
       To::N => match lane_number {
-        // take the first point of the lane, which is under index 2
+        // take the second point of the lane, which is under index 2
         0 => { self.no_autos_targeted_to_point(&self.way.ne[2], lane_autos) }/*turn right*/ 
         1 => { self.no_autos_targeted_to_point(&self.way.nn[2], lane_autos) }/*no turn*/
         _ => { self.no_autos_targeted_to_point(&self.way.nw[2], lane_autos) }/*turn left*/
@@ -97,6 +101,31 @@ impl<'a> Calc<'a> {
     
   }
 
+  fn auto_targeted_to_second_point_does_not_move(&self, road_direction: To, lane_number: u8, lane_autos:&LimitedStack<Auto>) -> bool {
+    match road_direction {
+      To::N => match lane_number {
+        // take the second point of the lane, which is under index 2
+        0 => { self.auto_targeted_to_point_does_not_move(&self.way.ne[2], lane_autos) }/*turn right*/
+        1 => { self.auto_targeted_to_point_does_not_move(&self.way.nn[2], lane_autos) }/*no turn*/
+        _ => { self.auto_targeted_to_point_does_not_move(&self.way.nw[2], lane_autos) }/*turn left*/
+      }
+      To::S => match lane_number {
+        0 => { self.auto_targeted_to_point_does_not_move(&self.way.sw[2], lane_autos) }
+        1 => { self.auto_targeted_to_point_does_not_move(&self.way.ss[2], lane_autos) }
+        _ => { self.auto_targeted_to_point_does_not_move(&self.way.se[2], lane_autos) }
+      }
+      To::W => match lane_number {
+        0 => { self.auto_targeted_to_point_does_not_move(&self.way.wn[2], lane_autos) }
+        1 => { self.auto_targeted_to_point_does_not_move(&self.way.ww[2], lane_autos) }
+        _ => { self.auto_targeted_to_point_does_not_move(&self.way.ws[2], lane_autos) }
+      }
+      _ => match lane_number { // To::E , east
+        0 => { self.auto_targeted_to_point_does_not_move(&self.way.es[2], lane_autos) }
+        1 => { self.auto_targeted_to_point_does_not_move(&self.way.ee[2], lane_autos) }
+        _ => { self.auto_targeted_to_point_does_not_move(&self.way.en[2], lane_autos) }
+      }
+    }
+  }
 
   fn lane_is_free(&self, road_direction:To, lane_number: u8) -> bool {
     // todo
@@ -128,17 +157,31 @@ impl<'a> Calc<'a> {
     no autos targeted to lane first point and there is auto targeted to second point, but this auto does not move, so this auto already in destination point (on second point), so no collision in add moment
      */
     self.no_autos_targeted_to_lane_first_point(road_direction, lane_number, lane_autos)
-    && self.no_autos_targeted_to_lane_second_point(road_direction, lane_number, lane_autos)
+    && (
+      self.no_autos_targeted_to_lane_second_point(road_direction, lane_number, lane_autos)
+      ||
+      self.auto_targeted_to_second_point_does_not_move(road_direction, lane_number, lane_autos)
+    ) //todo check it twice, does not look clear
   }
 
+  
+
   /** generate random number from 0 to 2 to choose the lane */
-  fn random_lane(road:To, skip: u8) -> u8 {
-    let lane_number = rand::thread_rng().gen_range(0..2);
-    if lane_number == skip {
-      Self::random_lane(road, skip)
-    } else {
-      lane_number
+  fn random_lane(&self, road:To) -> u8 {
+    let mut rng = rand::thread_rng();
+    let mut choices = vec![0, 1, 2];
+
+    while !choices.is_empty() {
+        let index = rng.gen_range(0..choices.len());
+        let lane_number = choices.remove(index);
+
+        if self.lane_is_free(road, lane_number) {
+            return lane_number;
+        }
     }
+
+    // If all choices are taken, return the (3) as a fail value
+    3
   }
 
   /** input handler call this method, to try add auto from south to north */
@@ -146,7 +189,7 @@ impl<'a> Calc<'a> {
     // self.autos.add_auto_north();
     println!("try_add_auto_north_directed calc.rs");
     // generate random number from 0 to 2 to choose the lane
-    match Self::random_lane(To::N, 3) {
+    match self.random_lane(To::N) {
       0 => println!("lane 0"),
       1 => println!("lane 1"),
       2 => println!("lane 2"),
